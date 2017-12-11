a68ca4576060d5af9ec47387ba335e54155f0726
Ryan Gosden <ryangosden@gmail.com>
2017-12-11 22:00:21 +0100

    dynamically added html elements

--- app/scripts/adverscroll.js
+++ app/scripts/adverscroll.js
@@ -1,16 +1,45 @@
 (function() {
   var adverscroll = {
     init: function() {
+      this.createElements();
       this.cacheDom();
       this.addMedia();
       this.addUrl();
     },
+    createElements: function(){
+      this.container = document.getElementById('inscroll-banner');
+
+      var inner = document.createElement("div");
+      inner.setAttribute("class", "isb-banner-core");
+      this.inner = inner;
+
+      var advert = document.createElement("div");
+      advert.setAttribute("class", "isb-banner-advert");
+      this.advert = inner;
+
+      var topLabel = document.createElement("div");
+      topLabel.setAttribute("class", "isb-label isb-label-top");
+      topLabel.appendChild(document.createTextNode("ADVERTISEMENT"));
+
+      var bottomLabel = document.createElement("div");
+      bottomLabel.setAttribute("class", "isb-label isb-label-bottom");
+      bottomLabel.appendChild(document.createTextNode("SCROLL TO CONTINUE"));
+
+      this.container.appendChild(topLabel);
+      this.container.appendChild(inner);
+      inner.appendChild(advert);
+      this.container.appendChild(bottomLabel);
+
+
+    },
     cacheDom: function() {
-        (this.container = document.getElementById("inscroll-banner")),
-        (this.inner = document.getElementById("banner-core")),
-        (this.advert = document.getElementById("inscroll-advert")),
-        (this.image = this.advert.getAttribute("data-image")),
-        (this.url = this.advert.getAttribute("data-url"));
+        // (this.container = document.getElementById("inscroll-banner")),
+        // (this.inner = document.getElementById("banner-core")),
+        // (this.advert = document.getElementById("inscroll-advert")),
+        // (this.image = this.advert.getAttribute("data-image")),
+        // (this.url = this.advert.getAttribute("data-url"));
+         this.image = '/images/adverscroll.jpg';
+         this.url = '/image/adverscroll.jpg';
     },
 
     appendMedia: function() {
@@ -44,3 +73,15 @@
 
   adverscroll.init();
 })();
+
+
+//
+// <!-- <div class="isb-label isb-label-top">ADVERTISEMENT</div>
+// <div class="isb-banner-core"id="banner-core">
+//   <div class="isb-banner-advert"
+//     id="inscroll-advert"
+//     data-image="/images/adverscroll.jpg"
+//     data-url="<?php echo $ad_takeover_url;?>">
+//   </div>
+// </div>
+// <div class="isb-label isb-label-bottom">SCROLL TO CONTINUE</div> -->
