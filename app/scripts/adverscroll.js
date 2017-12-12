00f793c34682e4678c220b49b0a115e99928e52e
Ryan Gosden <ryangosden@gmail.com>
2017-12-12 12:25:47 +0100

    image and url obtained from data-attribute

--- app/scripts/adverscroll.js
+++ app/scripts/adverscroll.js
@@ -2,7 +2,6 @@
   var adverscroll = {
     init: function() {
       this.createElements();
-      this.cacheDom();
       this.addMedia();
       this.addUrl();
     },
@@ -29,19 +28,22 @@
       this.container.appendChild(inner);
       inner.appendChild(advert);
       this.container.appendChild(bottomLabel);
-
-
     },
-    cacheDom: function() {
-        // (this.container = document.getElementById("inscroll-banner")),
-        // (this.inner = document.getElementById("banner-core")),
-        // (this.advert = document.getElementById("inscroll-advert")),
-        // (this.image = this.advert.getAttribute("data-image")),
-        // (this.url = this.advert.getAttribute("data-url"));
-         this.image = '/images/adverscroll.jpg';
-         this.url = '/image/adverscroll.jpg';
+    addMedia: function() {
+      var container = document.getElementById("inscroll-banner");
+      this.url = container.getAttribute("data-url");
+      this.image = container.getAttribute("data-img");
+      this.media = document.createElement("img");
+      this.media.setAttribute("src", this.image);
+      this.media.addEventListener("load", this.appendMedia.bind(this));
     },
-
+    addUrl: function() {
+      this.mediaUrl = document.createElement("a");
+      this.mediaUrl.setAttribute("href", this.url);
+      this.mediaUrl.setAttribute("target", "_blank");
+      this.mediaUrl.setAttribute("class", "isb-link");
+      this.advert.appendChild(this.mediaUrl);
+    },
     appendMedia: function() {
       this.advert.appendChild(this.media);
       this.enableScrollEvent();
@@ -56,32 +58,8 @@
       var pos = this.container.getBoundingClientRect();
       this.inner.style.clip =
         "rect(" + pos.top + "px,100vw," + pos.bottom + "px,0)";
-    },
-    addMedia: function() {
-      this.media = document.createElement("img");
-      this.media.setAttribute("src", this.image);
-      this.media.addEventListener("load", this.appendMedia.bind(this));
-    },
-    addUrl: function() {
-      this.mediaUrl = document.createElement("a");
-      this.mediaUrl.setAttribute("href", this.url);
-      this.mediaUrl.setAttribute("target", "_blank");
-      this.mediaUrl.setAttribute("class", "isb-link");
-      this.advert.appendChild(this.mediaUrl);
     }
   };
 
   adverscroll.init();
 })();
-
-
-//
-// <!-- <div class="isb-label isb-label-top">ADVERTISEMENT</div>
-// <div class="isb-banner-core"id="banner-core">
-//   <div class="isb-banner-advert"
-//     id="inscroll-advert"
-//     data-image="/images/adverscroll.jpg"
-//     data-url="<?php echo $ad_takeover_url;?>">
-//   </div>
-// </div>
-// <div class="isb-label isb-label-bottom">SCROLL TO CONTINUE</div> -->
