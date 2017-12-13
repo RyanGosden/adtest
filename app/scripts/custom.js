f8559bb01f91977737a3e336591e9a47d327f8f4
Ryan Gosden <ryangosden@gmail.com>
2017-12-13 10:39:26 +0100

    Added custom labels

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -2,5 +2,7 @@ var advert1 = Adverscroll({
   id : "ad1",
   image: "images/adverscroll.jpg",
   url: "www.hsbc.com.mt",
+  topLabelText: "ADVERTISEMENT",
+  bottomLabelText: "SCROLL TO CONTINUE"
 });
 advert1.apply();
