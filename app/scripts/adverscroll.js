bda0fc8258b31a2d83bafd0fbb5397335e22f6be
Ryan Gosden <ryangosden@gmail.com>
2017-12-31 14:28:06 +0100

    initial changes made

--- app/scripts/adverscroll.js
+++ app/scripts/adverscroll.js
@@ -1,164 +1,142 @@
 // @TODO
-
 // Fix multiple instances
 // Testing
-
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['adverscroll'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory(require('adverscroll'));
-  } else {
-    window.Adverscroll = factory();
-  }
+(function (root, factory) {
+    if(typeof exports === 'object' && typeof module === 'object')
+        module.exports = factory();
+    else if(typeof define === 'function' && define.amd)
+        define("adverscroll", [], factory);
+    else if(typeof exports === 'object')
+        exports["adverscroll"] = factory();
+    else
+        root["Adverscroll"] = factory();
 })(window, function() {
-  var Adverscroll = function(config) {
 
-    // Validation before creating object
-    // Accept only string values for config
-    for (const conf in config) {
-      if (typeof config[conf] !== 'string') {
-        throw 'Only strings are accepted in configuration.';
-      }
-    }
-
-    // Insure mediaUrl, mediaType, and url are set
-    if (
-      typeof config.mediaUrl === 'undefined' ||
-             config.mediaUrl.trim().length === 0 ||
-      typeof config.mediaType === 'undefined' ||
-             config.mediaType.trim().length === 0 ||
-      typeof config.url === 'undefined' ||
-             config.url.trim().length === 0
-    ) {
-      throw 'MediaUrl, MediaType and URL are required';
-    }
-
-    return new Adverscroll.init(config);
-  };
-
-  Adverscroll.init = function(config) {
-    this.id = config.id || 'adverscroll';
-    this.mediaUrl = config.mediaUrl || '';
-    this.mediaType = config.mediaType || '';
-    this.url = config.url || '';
-    this.topLabelText = config.topLabelText || 'ADVERTISEMENT';
-    this.bottomLabelText = config.bottomLabelText || 'SCROLL TO CONTINUE';
-    this.labelBgColour = config.labelBgColour || '#222';
-    this.labelColour = config.labelColour || '#fff';
-    this.marginTop = config.marginTop || 0;
-  };
-
-  Adverscroll.prototype = {
-    createContainer: function() {
-      this.container = document.getElementById(this.id);
-
-      var inner = document.createElement("div");
-      inner.setAttribute("class", "isb-banner-core");
-      this.inner = inner;
-
-      var advert = document.createElement("div");
-      advert.setAttribute("class", "isb-banner-advert");
-      if (this.marginTop !== 0) {
-        advert.style.marginTop = this.marginTop + "px";
-      }
-      this.advert = advert;
-      this.container.appendChild(inner);
-      inner.appendChild(advert);
-    },
-    createLabels: function() {
-      var topLabel = document.createElement("div"),
-          bottomLabel = document.createElement("div"),
-          labels = [
-            {
-              el: topLabel,
-              class: "isb-label isb-label-top",
-              txt: this.topLabelText
-            },
-            {
-              el: bottomLabel,
-              class: "isb-label isb-label-bottom",
-              txt: this.bottomLabelText
+    var Adverscroll = function(config) {
+        // Validation before creating object
+        // Accept only string values for config
+        for (var conf in config) {
+            if (typeof config[conf] !== 'string') {
+                throw 'Only strings are accepted in configuration.';
             }
-          ];
+        }
 
-      var createLabel = function(el, cls, txt) {
-        el.setAttribute("class", cls);
-        el.style.backgroundColor = this.labelBgColour;
-        el.style.color = this.labelColour;
-        el.appendChild(document.createTextNode(txt));
-        this.container.appendChild(el);
-      }.bind(this);
+        // Insure mediaUrl, mediaType, and url are set
+        if (
+            typeof config.mediaUrl === 'undefined' ||
+            config.mediaUrl.trim().length === 0 ||
+            typeof config.mediaType === 'undefined' ||
+            config.mediaType.trim().length === 0 ||
+            typeof config.url === 'undefined' ||
+            config.url.trim().length === 0
+        ) {
+            throw 'MediaUrl, MediaType and URL are required';
+        }
 
-      labels.forEach(function(label){
-          createLabel(label.el, label.class, label.txt);
-      });
-    },
-    checkMediaType: function() {
-      switch(this.mediaType) {
-        case 'image':
-          this.addImage();
-          break;
-        case 'video':
-          this.addVideo();
-          break;
-        case 'iframe':
-          this.addIframe();
-          break;
-      }
-    },
-    addImage: function() {
-      var media = document.createElement('img');
-      media.src = this.mediaUrl;
-      media.addEventListener('load', this.appendMedia(media));
-      this.addUrl();
-    },
-    addVideo: function() {
-      var media = document.createElement('video'),
-          source = document.createElement('source');
+        return new Adverscroll.init(config);
+    };
 
-      source.type = 'video/mp4';
-      source.src = this.mediaUrl;
-      media.appendChild(source);
-      media.setAttribute('controls', 'controls');
-      media.addEventListener('load', this.appendMedia(media));
-    },
-    addIframe: function() {
-      var media = document.createElement('iframe');
-      media.src = this.mediaUrl;
-      media.setAttribute('scrolling', 'no');
-      media.addEventListener('load', this.appendMedia(media));
-    },
-    addUrl: function() {
-      var url = document.createElement('a');
-      url.href = this.url;
-      url.target = '_blank';
-      url.setAttribute('class', 'isb-link');
-      this.advert.appendChild(url);
-    },
-    appendMedia: function(media) {
-      this.advert.appendChild(media);
-      this.enableScrollEvent();
-      this.render();
-    },
-    enableScrollEvent: function() {
-      document.body.onscroll = function() {
-        this.render();
-      }.bind(this);
-    },
-    render: function() {
-      var pos = this.container.getBoundingClientRect();
-      this.inner.style.clip =
-        'rect(' + pos.top + 'px,100vw,' + pos.bottom + 'px,0)';
-    },
-    apply: function() {
-      this.createContainer();
-      this.createLabels();
-      this.checkMediaType();
-    }
-  };
+    Adverscroll.init = function(config) {
+        this.id = config.id || 'adverscroll';
+        this.mediaUrl = config.mediaUrl || '';
+        this.mediaType = config.mediaType || '';
+        this.url = config.url || '';
+        this.topLabelText = config.topLabelText || 'ADVERTISEMENT';
+        this.bottomLabelText = config.bottomLabelText || 'SCROLL TO CONTINUE';
+        this.labelBgColour = config.labelBgColour || '#222';
+        this.labelColour = config.labelColour || '#fff';
+        this.marginTop = config.marginTop || 0;
+    };
 
-  Adverscroll.init.prototype = Adverscroll.prototype;
+    Adverscroll.prototype = {
+        createContainer: function() {
+            this.container = document.getElementById(this.id);
 
-  return Adverscroll;
+            var inner = document.createElement('div');
+            inner.setAttribute('class', 'isb-inner');
+            // this.inner = inner;
+
+            var clipper = document.createElement('div');
+            clipper.setAttribute('class', 'isb-clipper');
+            //this.clipper = clipper;
+
+            var content = document.createElement('div');
+            content.setAttribute('class', 'isb-content');
+            this.content = content;
+
+            // var advert = document.createElement('div');
+            // advert.setAttribute('class', 'isb-banner-advert');
+            // if (this.marginTop !== 0) {
+            //     advert.style.marginTop = this.marginTop + 'px';
+            // }
+            // this.advert = advert;
+            this.container.appendChild(inner);
+            inner.appendChild(clipper);
+            clipper.appendChild(this.content);
+        },
+        createLabels: function() {
+            var topLabel = document.createElement('div'),
+                bottomLabel = document.createElement('div'),
+                labels = [
+                    {
+                        el: topLabel,
+                        class: 'isb-label isb-label-top',
+                        txt: this.topLabelText
+                    },
+                    {
+                        el: bottomLabel,
+                        class: 'isb-label isb-label-bottom',
+                        txt: this.bottomLabelText
+                    }
+                ];
+
+            var createLabel = function(el, cls, txt) {
+                el.setAttribute('class', cls);
+                el.style.backgroundColor = this.labelBgColour;
+                el.style.color = this.labelColour;
+                el.appendChild(document.createTextNode(txt));
+                this.container.appendChild(el);
+            }.bind(this);
+
+            labels.forEach(function(label) {
+                createLabel(label.el, label.class, label.txt);
+            });
+        },
+        checkMediaType: function() {
+            switch (this.mediaType) {
+                case 'image':
+                    this.addImage();
+                    break;
+                case 'iframe':
+                    this.addIframe();
+                    break;
+            }
+        },
+        addImage: function() {
+            var media = document.createElement('img');
+            media.src = this.mediaUrl;
+            media.addEventListener('load', this.appendMedia(media));
+            this.addUrl();
+        },
+        addUrl: function() {
+            var url = document.createElement('a');
+            url.href = this.url;
+            url.target = '_blank';
+            url.setAttribute('class', 'isb-link');
+            this.advert.appendChild(url);
+        },
+        appendMedia: function(media) {
+            this.container.appendChild(media);
+            // this.enableScrollEvent();
+        },
+        apply: function() {
+            this.createContainer();
+            this.createLabels();
+            this.checkMediaType();
+        }
+    };
+
+    Adverscroll.init.prototype = Adverscroll.prototype;
+
+    return Adverscroll;
 });
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,20 +1,7 @@
-var advert1 = Adverscroll({
-  id : "adverscroll1",
-  mediaUrl: "https://fr.wikipedia.org/wiki/Main_Page",
-  mediaType: "iframe",
-  url: "www.hsbc.com.mt",
-  topLabelText: "ADVERTISEMENT",
-  bottomLabelText: "SCROLL TO CONTINUE",
-  labelBgColour: "#222",
-  labelColour: "#fff",
-  marginTop: "0",
-});
-advert1.apply();
-
-// var advert2 = Adverscroll({
-//   id : "adverscroll2",
-//   mediaUrl: "images/adverscroll.jpg",
-//   mediaType: "image",
+// var advert1 = Adverscroll({
+//   id : "adverscroll1",
+//   mediaUrl: "https://fr.wikipedia.org/wiki/Main_Page",
+//   mediaType: "iframe",
 //   url: "www.hsbc.com.mt",
 //   topLabelText: "ADVERTISEMENT",
 //   bottomLabelText: "SCROLL TO CONTINUE",
@@ -22,7 +9,20 @@ advert1.apply();
 //   labelColour: "#fff",
 //   marginTop: "0",
 // });
-// advert2.apply();
+// advert1.apply();
+
+var advert2 = Adverscroll({
+  id : "adverscroll",
+  mediaUrl: "images/adverscroll.jpg",
+  mediaType: "image",
+  url: "www.hsbc.com.mt",
+  topLabelText: "ADVERTISEMENT",
+  bottomLabelText: "SCROLL TO CONTINUE",
+  labelBgColour: "#222",
+  labelColour: "#fff",
+  marginTop: "0",
+});
+advert2.apply();
 //
 // var advert1 = Adverscroll({
 //   id : "adverscroll",
